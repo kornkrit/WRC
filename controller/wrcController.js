ba9ac39162fa9f1621b1f026bae9ea48fa3d19de
kornkritsupayanant <kornkrit.sup@gmail.com>
2017-03-06 00:29:06 +0700

    Enhance score and add caseSearch Attribute

--- controller/wrcController.js
+++ controller/wrcController.js
@@ -15,32 +15,40 @@ function findWordCount(word){
     wordSet.sort();
     var current = null;
     var count = 0;
-
-    for (var i = 0; i < wordSet.length; i++) {
-        if (wordSet[i] != current) {
-            if (count > 0) {
-                result.push({ "word" : current, "wordCount" : count});
+    if(wordSet.length == 1){
+        result.push({"word": wordSet[0], "wordCount": 1});
+    }else {
+        for (var i = 0; i < wordSet.length; i++) {
+            if (wordSet[i] != current) {
+                if (count > 0) {
+                    result.push({"word": current, "wordCount": count});
+                }
+                current = wordSet[i];
+                count = 1;
+            } else {
+                count++;
             }
-            current = wordSet[i];
-            count = 1;
-        } else {
-            count++;
         }
-    }
-    if (count > 0) {
-        result.push({ "word" : current, "wordCount" : count});
+        if (count > 0) {
+            result.push({"word": current, "wordCount": count});
+        }
     }
     return result;
 }
 
 function findScore(testSet,trainSet){
     var resultObj = new Array();
+    console.log(testSet.length + ":" + trainSet.length);
     for(var i = 0; i < testSet.length; i++){
         var chk = 0;
         for(var j = 0; j < trainSet.length; j++){
-            if(testSet[i].word.toLowerCase() == trainSet[j].word.toLowerCase()){
+
+            var xx = testSet[i].word.toLowerCase() === trainSet[j].word.toLowerCase();
+            if(xx){
+                console.log("Logic " + xx);
+                console.log("Logic " + testSet[i].word.toLowerCase() +":"+trainSet[j].word.toLowerCase());
                 resultObj.push({"word" : testSet[i].word, "testWordCount" : testSet[i].wordCount,"trainWordCount" : trainSet[j].wordCount});
-                break;
+                //break;
             }
             else{
                 chk++;
@@ -52,15 +60,15 @@ function findScore(testSet,trainSet){
 
         }
     }
-
-    var x = 0,y1 = 1, y2 = 1;
+    console.log(resultObj.toString());
+    var x = 0,y1 = 0, y2 = 0;
     for(var i = 0; i < resultObj.length; i++){
         x += (resultObj[i].testWordCount * resultObj[i].trainWordCount);
         y1 += (resultObj[i].testWordCount * resultObj[i].testWordCount);
         y2 += (resultObj[i].trainWordCount * resultObj[i].trainWordCount);
-        //print(resultObj[i].word + ":" + resultObj[i].testWordCount + "*" + resultObj[i].trainWordCount + "," + resultObj[i].testWordCount + "*" + resultObj[i].testWordCount + "," + resultObj[i].trainWordCount +"*"+ resultObj[i].trainWordCount);
     }
     var result = x/(( Math.sqrt(y1) * Math.sqrt(y2)));
+    console.log("Result: " + result);
     return result;
 }
 
@@ -73,7 +81,7 @@ module.exports = {
         for(var i = 0; i < oraSet.length; i++){
             testObj.push(findWordCount(oraSet[i].ora_id));
             for(var j = 0; j < (testObj[i]).length; j++) {
-            if (i == 0 && j ==0)
+            if (i == 0 && j == 0)
                 queryStr += 'WHERE ora_id LIKE \'%' + (testObj[i])[j].word + '%\'';
             else
                 queryStr += 'OR ora_id LIKE \'%' + (testObj[i])[j].word + '%\'';
@@ -83,7 +91,6 @@ module.exports = {
         db.getConnection(function (err, conn) {
             conn.query(queryStr, function (err, rows) {
                 var trainResult = new Array();
-                //console.log(rows);
                 if (!err && rows.length > 0){
                     for(var j = 0; j< rows.length; j++){
                         trainResult.push({"ID" : j,"obj" : findWordCount(rows[j].ora_id)});
@@ -91,19 +98,19 @@ module.exports = {
                     for(var i = 0; i < testObj.length; i++){
                         for(var j = 0; j < trainResult.length; j++){
                             var score = findScore(testObj[i],trainResult[j].obj);
-                            if(score > 0.5)
-                            result.push({"word" : [{ "caused" : rows[trainResult[j].ID].caused ,"actions": rows[trainResult[j].ID].actions, "score" : (Math.round(score * 100) / 100) }]});
+                            if(score > 0.8)
+                            result.push({"word" : [{ "caseSearch" : rows[trainResult[j].ID].ora_id, "caused" : rows[trainResult[j].ID].caused ,"actions" : rows[trainResult[j].ID].actions, "score" : (Math.round(score * 100) / 100) }]});
                         }
                     }
                     return callback(null,{"Results" : [{"KeySearch" : oraSet, "ObjResults" : result}]});
                 }else if(rows.length == 0 ){
                     console.error("It is empty.");
                     result.push({"Results" : null});
-                    return callback(null,{"Results" : [{"KeySearch" : oraSet, "ObjResults" : result}]});
+                    return callback(null,{"Results" : [{"KeySearch" : oraSet, "ObjResults" : null}]});
                 }else {
                     console.error('<--- Something went error --->');
                     console.error(err);
-                    return callback(err,err);
+                    return callback(err,{"Results" : [{"KeySearch" : oraSet, "ObjResults" : null}]});
                 }
             });
             conn.release();
